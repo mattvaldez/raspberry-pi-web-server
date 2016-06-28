1f28147c3406e8a852a9f47c84813a9272e31771
matt valdez <matvaldez@gmail.com>
2016-06-28 02:49:34 +0000

    hello world get and post

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
-app.get('/:name', function(req, res) {
-  var name = req.param('name');  
-  res.send('hello ' + name);
-});
-
 app.use(bodyParser.json()); //support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); //support encoded bodies
 
+app.get('/', function(req, res){
+  res.send('hell0 world');
+})
 app.post('/', function(req, res){
-  var name = {'name': req.body.name};
-  res.json(name);
+  var color = req.body;
+  res.json(color);
 })
 app.listen(8000, function () {
   console.log('Hello from the Raspberry Pi!');
